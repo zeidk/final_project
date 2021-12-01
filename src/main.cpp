8edc8522542187eaee6202b8382f63be8cd400d2
zeidk <zeid.kootbally@nist.gov>
2021-11-30 23:08:15 -0500

    First push

---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <actionlib/client/simple_action_client.h>
+#include <fiducial_msgs/FiducialTransformArray.h>
+#include <geometry_msgs/Twist.h>  //for geometry_msgs::Twist
+#include <move_base_msgs/MoveBaseAction.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <ros/ros.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+
+void broadcast() {
+  //for broadcaster
+  static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+
+  //broadcast the new frame to /tf Topic
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = "explorer_tf/camera_rgb_optical_frame";
+  transformStamped.child_frame_id = "my_frame";
+
+  transformStamped.transform.translation.x = 0.5;
+  transformStamped.transform.translation.y = 0.5;
+  transformStamped.transform.translation.z = 0.2;
+  transformStamped.transform.rotation.x = 0;
+  transformStamped.transform.rotation.y = 0;
+  transformStamped.transform.rotation.z = 0;
+  transformStamped.transform.rotation.w = 1;
+  ROS_INFO("Broadcasting");
+  br.sendTransform(transformStamped);
+}
+
+void listen(tf2_ros::Buffer& tfBuffer) {
+  //for listener
+  
+  geometry_msgs::TransformStamped transformStamped;
+  try {
+    transformStamped = tfBuffer.lookupTransform("map", "my_frame", ros::Time(0));
+    auto trans_x = transformStamped.transform.translation.x;
+    auto trans_y = transformStamped.transform.translation.y;
+    auto trans_z = transformStamped.transform.translation.z;
+
+    ROS_INFO_STREAM("Position in map frame: ["
+      << trans_x << ","
+      << trans_y << ","
+      << trans_z << "]"
+    );
+  }
+  catch (tf2::TransformException& ex) {
+    ROS_WARN("%s", ex.what());
+    ros::Duration(1.0).sleep();
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "simple_navigation_goals");
+  ros::NodeHandle nh;
+
+  // tell the action client that we want to spin a thread by default
+  MoveBaseClient explorer_client("/explorer/move_base", true);
+  // tell the action client that we want to spin a thread by default
+  MoveBaseClient follower_client("/follower/move_base", true);
+
+  // wait for the action server to come up
+  while (!explorer_client.waitForServer(ros::Duration(5.0))) {
+    ROS_INFO("Waiting for the move_base action server to come up for explorer");
+  }
+
+  while (!follower_client.waitForServer(ros::Duration(5.0))) {
+    ROS_INFO("Waiting for the move_base action server to come up for follower");
+  }
+
+  move_base_msgs::MoveBaseGoal explorer_goal;
+  move_base_msgs::MoveBaseGoal follower_goal;
+
+  //Build goal for explorer
+  explorer_goal.target_pose.header.frame_id = "map";
+  explorer_goal.target_pose.header.stamp = ros::Time::now();
+  explorer_goal.target_pose.pose.position.x = 7.710214;//
+  explorer_goal.target_pose.pose.position.y = -1.716889;//
+  explorer_goal.target_pose.pose.orientation.w = 1.0;
+
+  //Build goal for follower
+  follower_goal.target_pose.header.frame_id = "map";
+  follower_goal.target_pose.header.stamp = ros::Time::now();
+  follower_goal.target_pose.pose.position.x = -0.289296;//
+  follower_goal.target_pose.pose.position.y = -1.282680;//
+  follower_goal.target_pose.pose.orientation.w = 1.0;
+
+  ROS_INFO("Sending goal");
+  explorer_client.sendGoal(explorer_goal);
+  // explorer_client.waitForResult();
+
+  ROS_INFO("Sending goal");
+  // follower_client.sendGoal(follower_goal);
+  // explorer_client.waitForResult();
+
+
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+  ros::Rate loop_rate(10);
+
+  while (ros::ok()) {
+    if (explorer_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Hooray, robot reached goal");
+    }
+
+    if (follower_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Hooray, robot reached goal");
+    }
+    broadcast();
+    listen(tfBuffer);
+    loop_rate.sleep();
+  }
+
+
+}
